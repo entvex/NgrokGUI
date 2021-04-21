6e7bf4b454108b3b3360f54151947ea9aaf0f690
entvex <entvex@gmail.com>
2021-04-21 15:21:10 +0200

    Initial v2 commit

---
+++ NgrokGUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NgrokGUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
